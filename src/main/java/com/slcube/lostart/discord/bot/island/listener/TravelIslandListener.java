9ca27a4107c002b34a11cf9908a8bdc216cbaf67
SLCube <kgm0096@naver.com>
2024-11-22 17:29:56 +0900

    feat: 매일 오전10시30분에 모험섬 알리미가 작동하는 기능 추가

--- src/main/java/com/slcube/lostart/discord/bot/island/listener/TravelIslandListener.java
+++ src/main/java/com/slcube/lostart/discord/bot/island/listener/TravelIslandListener.java
@@ -4,20 +4,27 @@ import com.slcube.lostart.discord.bot.island.service.LostArkIslandScheduleServic
 import com.slcube.lostart.discord.bot.island.service.dto.TravelIslandDto;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class TravelIslandListener extends ListenerAdapter {
@@ -43,21 +50,57 @@ public class TravelIslandListener extends ListenerAdapter {
 
         String msg = message.getContentDisplay();
         if (msg.equals("!모험섬")) {
-            List<TravelIslandDto> travelIslandList = service.getIslandList(today);
-            String receiveMessage = "";
-            if (travelIslandList.isEmpty()) {
-                receiveMessage += "모험섬 정보가 없습니다.";
-            } else {
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
-                String dayOfWeek = today.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREAN);
-
-                receiveMessage = receiveMessage.concat(formatter.format(today) + " ").concat(dayOfWeek + " ").concat("모험 섬 정보입니다.\n");
-
-                for (TravelIslandDto travelIsland : travelIslandList) {
-                    receiveMessage = receiveMessage.concat(travelIsland.getTimeType().getTimeType() + " ").concat(travelIsland.getIslandName()).concat(" : ").concat(travelIsland.getRewardType()).concat("\n");
-                }
-            }
+            String receiveMessage = getTravelIslandReceiveMessage(today);
             textChannel.sendMessage(receiveMessage).queue();
         }
+    }
+
+
+    @Override
+    public void onReady(ReadyEvent event) {
+
+        TextChannel channel = event.getJDA().getTextChannelById(targetChannelId);
+        if (channel != null) {
+            long initialDelay = calculateInitialDelay();
+            long period = TimeUnit.MINUTES.toMillis(1);
+            LocalDate today = LocalDate.now();
+            event.getJDA().getGatewayPool().scheduleAtFixedRate(
+                    () -> channel.sendMessage(getTravelIslandReceiveMessage(today)).queue(),
+                    initialDelay,
+                    period,
+                    TimeUnit.MILLISECONDS);
+        }
+    }
+
+    private long calculateInitialDelay() {
+        LocalDateTime now = LocalDateTime.now();
+
+        LocalDateTime targetTime = now.withHour(10).withMinute(30).withSecond(0).withNano(0);
+        if (now.isAfter(targetTime)) {
+            targetTime = targetTime.plusDays(1); // 내일 오전 10시 30분
+        }
+
+        // 현재 시간과 목표 시간 사이의 차이를 밀리초 단위로 계산
+        return ChronoUnit.MILLIS.between(now, targetTime);
+    }
+
+    @NotNull
+    private String getTravelIslandReceiveMessage(LocalDate today) {
+        List<TravelIslandDto> travelIslandList = service.getIslandList(today);
+        String receiveMessage = "";
+        if (travelIslandList.isEmpty()) {
+            receiveMessage += "모험섬 정보가 없습니다.";
+        } else {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
+            String dayOfWeek = today.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREAN);
+
+            receiveMessage = receiveMessage.concat(formatter.format(today) + " ").concat(dayOfWeek + " ").concat("모험 섬 정보입니다.\n");
+
+            for (TravelIslandDto travelIsland : travelIslandList) {
+                receiveMessage = receiveMessage.concat(travelIsland.getTimeType().getTimeType() + " ").concat(travelIsland.getIslandName()).concat(" : ").concat(travelIsland.getRewardType()).concat("\n");
+            }
+        }
+        log.info("receiveMessage : {}", receiveMessage);
+        return receiveMessage;
     }
 }
